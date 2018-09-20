896e1b9c75ded60a563d053cfbaf0e7042e0315b
Ray_Bruges <ogurezslava@gmail.com>
2018-09-20 23:11:19 +0300

    json files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let counter=0;
+let chckbr=true;
+
+	for(let i=0; i<bracketsConfig.length; i++){
+		for(let j=0; j<str.length; j++){
+			if(str[j]==bracketsConfig[i][0]){
+				counter++;
+			}
+			else if(str[j]==bracketsConfig[i][1]){
+			counter--;
+}
+		 if(counter<0){
+			 chckbr=false;
+			 break;
+		 }
+		}
+	}
+	if(counter>0 || chckbr<0){
+		chckbr=false;}
+		else {
+			chckbr=true;
+		}
+		return chckbr;
 }
